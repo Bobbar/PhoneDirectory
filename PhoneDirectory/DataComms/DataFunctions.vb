ad7256e12e4a6d3c17ac917e60583e86e834da48
rl12184 <rblovell@fairfielddd.com>
2017-06-21 16:01:04 -0400

    Added correct directory structure for SQLite dependent interop.dll. Allows working ClickOnce deployment.

--- PhoneDirectory/DataComms/DataFunctions.vb
+++ PhoneDirectory/DataComms/DataFunctions.vb
@@ -15,11 +15,16 @@ Module DataFunctions
     Public Sub RefreshLocalDBCache()
         Try
             Dim tsk = Task.Run(Sub()
-                                   BuildingCache = True
-                                   Using conn As New SQLite_Comms(False)
-                                       conn.RefreshSQLCache()
-                                   End Using
-                                   BuildingCache = False
+                                   Try
+                                       BuildingCache = True
+                                       Using conn As New SQLite_Comms(False)
+                                           conn.RefreshSQLCache()
+                                       End Using
+                                       BuildingCache = False
+                                   Catch ex As Exception
+                                       BuildingCache = False
+                                       ErrHandle(ex, System.Reflection.MethodInfo.GetCurrentMethod())
+                                   End Try
                                End Sub)
         Catch ex As Exception
             BuildingCache = False
